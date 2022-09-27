1e0359066def51717a25c7c41d8ef933022ac90a
Jomamer <82544820+Jomamer@users.noreply.github.com>
2022-09-27 07:21:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,23 +4,23 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import linear_model, svm
 
 st.write('''
-# Prédiction des orientations académiques
-Cette application prédit l'aptitude, la série et la moyenne annuelle de l'apprenant''')
+# Prediction of academic orientations
+This application predicts the learner's ability, series and yearly average in 10th grade''')
 
-st.sidebar.header("Insérer les moyennes de l'apprenant")
+st.sidebar.header("Insert learner's averages from 6th grade to 9th grade")
 def user_input():
-        mm6 = st.sidebar.slider('Maths 6e', 0, 20, 10)
-        mpc6 = st.sidebar.slider('PCT 6e', 0, 20, 10)
-        ms6 = st.sidebar.slider('SVT 6e', 0, 20, 10)
-        mm5 = st.sidebar.slider('Math 5e', 0, 20, 10)
-        mpc5 = st.sidebar.slider('PCT 5e', 0, 20, 10)
-        ms5 = st.sidebar.slider('SVT 5e', 0, 20, 10)
-        mm4 = st.sidebar.slider('Maths 4e', 0, 20, 10)
-        mpc4 = st.sidebar.slider('PCT 4e', 0, 20, 10)
-        ms4 = st.sidebar.slider('SVT 4e', 0, 20, 10)
-        mm3 = st.sidebar.slider('Maths 3e', 0, 20, 10)
-        mpc3 = st.sidebar.slider('PCT 3e', 0, 20, 10)
-        ms3 = st.sidebar.slider('SVT 3e', 0, 20, 10)
+        mm6 = st.sidebar.slider('Maths 6th grade', 0, 20, 10)
+        mpc6 = st.sidebar.slider('PCT 6th grade', 0, 20, 10)
+        ms6 = st.sidebar.slider('SVT 6th grade', 0, 20, 10)
+        mm5 = st.sidebar.slider('Math 7th', 0, 20, 10)
+        mpc5 = st.sidebar.slider('PCT 7th grade', 0, 20, 10)
+        ms5 = st.sidebar.slider('SVT 7th grade', 0, 20, 10)
+        mm4 = st.sidebar.slider('Maths 8th grade', 0, 20, 10)
+        mpc4 = st.sidebar.slider('PCT 8th grade', 0, 20, 10)
+        ms4 = st.sidebar.slider('SVT 8th grade', 0, 20, 10)
+        mm3 = st.sidebar.slider('Maths 9th grade', 0, 20, 10)
+        mpc3 = st.sidebar.slider('PCT 9th grade', 0, 20, 10)
+        ms3 = st.sidebar.slider('SVT 9th grade', 0, 20, 10)
         moyennes = {
             'mm6': mm6,
             'mpc6': mpc6,
@@ -40,7 +40,7 @@ def user_input():
 
 df = user_input()
 
-st.subheader('Moyennes insérées')
+st.subheader('Averages inserted')
 st.write(df)
 
 dataa = pd.read_csv('MoyennesBinSeptembre22.csv',encoding='latin-1')
@@ -70,14 +70,14 @@ predictionm = modelm.predict(df)
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.header("Aptitude :")
+    st.header("Ability :")
     if predictiona == 0:
-        st.write('Littéraire')
+        st.write('Literary')
     else:
-        st.write('Scientifique')
+        st.write('Scientific')
 
 with col2:
-    st.header("Séries:")
+    st.header("Series:")
     if predictions == 0:
         st.write('A')
     if predictions == 1:
@@ -86,6 +86,6 @@ with col2:
         st.write('C')
 
 with col3:
-    st.header('Moyenne annuelle:')
+    st.header('Yearly average in 10th:')
     st.write(predictionm)
 

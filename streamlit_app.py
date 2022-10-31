1bb58f217b2c3c2661c1e5f0898abd726d44465a
Jomamer <82544820+Jomamer@users.noreply.github.com>
2022-10-31 00:05:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,22 +5,22 @@ from sklearn import linear_model, svm
 
 st.write('''
 # Prediction of academic orientations
-This application predicts the learner's ability, classes and yearly average in 10th grade''')
+This application predicts the learner's ability and learner's stream''')
 
 st.sidebar.header("Insert learner's averages from 6th grade to 9th grade")
 def user_input():
-        mm6 = st.sidebar.slider('Maths 6th grade', 0, 20, 10)
+        mm6 = st.sidebar.slider('Mathematics 6th grade', 0, 20, 10)
         mpc6 = st.sidebar.slider('PCT 6th grade', 0, 20, 10)
-        ms6 = st.sidebar.slider('LES 6th grade', 0, 20, 10)
-        mm5 = st.sidebar.slider('Math 7th', 0, 20, 10)
+        ms6 = st.sidebar.slider('Biology 6th grade', 0, 20, 10)
+        mm5 = st.sidebar.slider('Mathematics 7th', 0, 20, 10)
         mpc5 = st.sidebar.slider('PCT 7th grade', 0, 20, 10)
-        ms5 = st.sidebar.slider('LES 7th grade', 0, 20, 10)
-        mm4 = st.sidebar.slider('Maths 8th grade', 0, 20, 10)
+        ms5 = st.sidebar.slider('Biology 7th grade', 0, 20, 10)
+        mm4 = st.sidebar.slider('Mathematics 8th grade', 0, 20, 10)
         mpc4 = st.sidebar.slider('PCT 8th grade', 0, 20, 10)
-        ms4 = st.sidebar.slider('LES 8th grade', 0, 20, 10)
-        mm3 = st.sidebar.slider('Maths 9th grade', 0, 20, 10)
+        ms4 = st.sidebar.slider('Biology 8th grade', 0, 20, 10)
+        mm3 = st.sidebar.slider('Mathematics 9th grade', 0, 20, 10)
         mpc3 = st.sidebar.slider('PCT 9th grade', 0, 20, 10)
-        ms3 = st.sidebar.slider('LES 9th grade', 0, 20, 10)
+        ms3 = st.sidebar.slider('Biology 9th grade', 0, 20, 10)
         moyennes = {
             'mm6': mm6,
             'mpc6': mpc6,
@@ -45,7 +45,6 @@ st.write(df)
 
 dataa = pd.read_csv('MoyennesBinSeptembre22.csv',encoding='latin-1')
 datas = pd.read_csv('MoyennesBinairesSeptembre22.csv',encoding='latin-1')
-datam = pd.read_csv('MoyennesFinalesSeptembre22.csv',encoding='latin-1')
 
 Xa = dataa.iloc[:, 0:12]
 ya = dataa.iloc[:, 13]
@@ -62,12 +61,10 @@ modela = rfc.fit(Xa, ya)
 models = lsvc.fit(Xs, ys)
 modelm = lr.fit(Xm, ym)
 
-
 predictiona = modela.predict(df)
 predictions = models.predict(df)
-predictionm = modelm.predict(df)
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
     st.header("Ability :")
@@ -77,15 +74,12 @@ with col1:
         st.write('Scientific')
 
 with col2:
-    st.header("Classes:")
+    st.header("Streams:")
     if predictions == 0:
-        st.write('A')
+        st.write('Literature stream')
     if predictions == 1:
-        st.write('D')
+        st.write('Biology stream')
     if predictions == 2:
-        st.write('C')
+        st.write('MPS stream')
 
-with col3:
-    st.header('Yearly average in 10th:')
-    st.write(predictionm)
 
